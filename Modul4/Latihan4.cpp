985c7a7a29004d41891cd0f872bc17a6fdc9ed6a
Aldi Maulana Bahari <106325150+aldimaulana1605@users.noreply.github.com>
2022-05-26 23:22:10 +0700

    Add files via upload

---
+++ Modul4/Latihan4.cpp
@@ -0,0 +1,18 @@
+/*====================================================
+  program jumlah
+  Modul 4 -> latihan4.cpp
+  Nama: Aldi Maulana Bahari
+  NPM : 2021804088
+  ====================================================*/
+
+#include<iostream>
+
+using namespace std;
+int main(){
+int x,y,jumlah;
+x = 100;               
+y = 50;
+jumlah = x + y;      
+cout<<x<<" + "<<y<<" = "<< jumlah<<endl;
+return 0;
+}

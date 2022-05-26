915646faccca7e1e11f47a8862e4c728c1ba4e86
Aldi Maulana Bahari <106325150+aldimaulana1605@users.noreply.github.com>
2022-05-26 22:15:54 +0700

    Add files via upload

---
+++ Modul2/Latihan2.cpp
@@ -0,0 +1,22 @@
+/*====================================================
+  Program Tampil Pesan
+  Modul 2 -> latihan2.cpp
+  Nama: 
+  NPM : 
+  ====================================================*/
+
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Selamat Datang di Program C++"<<endl;
+	cout <<"-----------------------------"<<endl;
+	cout <<"Nama Siswa : Aldi Maulana Bahari"<<endl;
+	cout <<"Semester   : 2"<<endl;
+	cout <<"Jurusan    : Sistem Informasi"<<endl;
+	return 0;
+
+}

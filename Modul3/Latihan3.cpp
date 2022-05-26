6b28e007511b8e273166561781cd3dd71549396c
Aldi Maulana Bahari <106325150+aldimaulana1605@users.noreply.github.com>
2022-05-26 22:16:10 +0700

    Add files via upload

---
+++ Modul3/Latihan3.cpp
@@ -0,0 +1,17 @@
+/*====================================================
+  Program Tampil
+  Modul 3 -> latihan3.cpp
+  Nama: Aldi Maulana Bahari
+  NPM : 2021804088
+  ====================================================*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout <<"Bahasa Pemrograman C++"<<endl;
+	cout <<"STMIK Insan Pembangunan"<<endl;
+	return 0;
+}

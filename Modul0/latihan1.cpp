2d4c730714f156775786f2170ea5e3ff09d6423a
Aldi Maulana Bahari <106325150+aldimaulana1605@users.noreply.github.com>
2022-06-10 17:40:42 +0700

    Add files via upload

---
+++ Modul0/latihan1.cpp
@@ -0,0 +1,15 @@
+/*====================================================
+  Program pertama Microsoft Visual C++ 2008
+  Modul0 -> latihan0.cpp
+  Nama: Aldi Maulana Bahari
+  NPM : 2021804088
+  ====================================================*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout <<"Ini adalah program pertama saya\n";
+	std::cout <<"dengan mengunakan MS Visual C++\n";
+	return 0;
+}
